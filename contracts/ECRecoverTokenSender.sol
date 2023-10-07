449f0704a6c6414c8c368137f7a9628192b06bbd
SilenceHide <silencehide.021@gmail.com>
2023-10-07 11:49:30 +0330

    Practice

--- contracts/ECRecoverTokenSender.sol
+++ contracts/ECRecoverTokenSender.sol
@@ -4,8 +4,9 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "contracts/Mytoken.sol";
 
 contract TokenSender {
 
-    
+    using ECDSA for bytes32;
 }
